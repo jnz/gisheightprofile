087732047b5b0457d4c99a59cd332d8a5abe9545
Jan Zwiener <jan.zwiener@gmail.com>
2011-05-16 19:30:57 +0200

    Formatting

--- heightprofile.js
+++ heightprofile.js
@@ -1,45 +1,37 @@
+var mapPanel;
+
 Ext.onReady(function () {
     var map = new OpenLayers.Map();
-    //var layer = new OpenLayers.Layer.WMS(
-    //    "Global Imagery",
-    //    "http://maps.opengeo.org/geowebcache/service/wms",
-    //    {layers: "bluemarble"}
-    //    );
-    //map.addLayer(layer);
 
-    var gmap = new OpenLayers.Layer.Google(
-        "Google Streets", // the default
-        {numZoomLevels: 20}
-        // default type, no change needed here
-        );
-    var gphy = new OpenLayers.Layer.Google(
-        "Google Physical",
-        {type: google.maps.MapTypeId.TERRAIN}
-        // used to be {type: G_PHYSICAL_MAP}
-        );
+    var gmap = new OpenLayers.Layer.Google("Google Streets",
+                                           { numZoomLevels: 20 });
+    var gphy = new OpenLayers.Layer.Google("Google Physical",
+                                           { type: google.maps.MapTypeId.TERRAIN });
     var osm = new OpenLayers.Layer.OSM();
-    var ghyb = new OpenLayers.Layer.Google(
-        "Google Hybrid",
-        {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}
+    var ghyb = new OpenLayers.Layer.Google("Google Hybrid",
+        {
+            type: google.maps.MapTypeId.HYBRID,
+            numZoomLevels: 20
+        }
         // used to be {type: G_HYBRID_MAP, numZoomLevels: 20}
-        );
-    var gsat = new OpenLayers.Layer.Google(
-            "Google Satellite",
-            {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
-            // used to be {type: G_SATELLITE_MAP, numZoomLevels: 22}
-            );
+    );
+    var gsat = new OpenLayers.Layer.Google("Google Satellite", {
+        type: google.maps.MapTypeId.SATELLITE,
+        numZoomLevels: 22
+    }
+    // used to be {type: G_SATELLITE_MAP, numZoomLevels: 22}
+    );
 
-    map.addLayers([gmap,gphy,osm, ghyb, gsat]);
-    map.addControl( new OpenLayers.Control.LayerSwitcher);
+    map.addLayers([gmap, gphy, osm, ghyb, gsat]);
+    map.addControl(new OpenLayers.Control.LayerSwitcher);
 
     mapPanel = new GeoExt.MapPanel({
         map: map,
-             region: 'center',
-             zoom: 6,
-             tbar: new Ext.Toolbar(),
-             center:new OpenLayers.LonLat(11.019287, 51.041394).transform(
-                 new OpenLayers.Projection("EPSG:4326"),
-                 map.getProjectionObject())
+        region: 'center',
+        zoom: 6,
+        tbar: new Ext.Toolbar(),
+        center: new OpenLayers.LonLat(11.019287, 51.041394).transform(
+        new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject())
     });
 
     new Ext.Panel({
@@ -47,12 +39,20 @@ Ext.onReady(function () {
         layout: 'border',
         renderTo: 'gxmap',
         height: 600,
+        autoHeight: true,
         //width: 800,
         tbar: new Ext.Toolbar({
-            items: [
-        {text: 'func1', handler: function() { alert('func1'); } },
-        {text: 'func2', handler: function() { alert('func2'); } },
-        ]
+            items: [{
+                text: 'func1',
+                handler: function () {
+                    alert('func1');
+                }
+            }, {
+                text: 'func2',
+                handler: function () {
+                    alert('func2');
+                }
+            }, ]
         }),
         items: [mapPanel]
     });
@@ -60,19 +60,19 @@ Ext.onReady(function () {
     //just a litte test (Paul)
     var length = new OpenLayers.Control.Measure(OpenLayers.Handler.Path, {
         eventListeners: {
-                            measure: function(evt) {
-                                         alert("The length was " + evt.measure + evt.units);
-                                     }
-                        }
+            measure: function (evt) {
+                alert("The length was " + evt.measure + evt.units);
+            }
+        }
     });
 
 
     var area = new OpenLayers.Control.Measure(OpenLayers.Handler.Polygon, {
         eventListeners: {
-                            measure: function(evt) {
-                                         alert("The area was " + evt.measure + evt.units);
-                                     }
-                        }
+            measure: function (evt) {
+                alert("The area was " + evt.measure + evt.units);
+            }
+        }
     });
 
     mapPanel.map.addControl(length);
@@ -110,6 +110,5 @@ Ext.onReady(function () {
 
     mapPanel.getTopToolbar().addButton(lengthButton);
     mapPanel.getTopToolbar().addButton(areaButton);
-
 });
 
