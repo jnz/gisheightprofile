1b3e237f08ccb91a7a81af6a81c045ea56493ac9
Jan Zwiener <jan.zwiener@gmail.com>
2011-05-16 11:09:17 +0200

    quick test

--- heightprofile.js
+++ heightprofile.js
@@ -5,44 +5,44 @@ Ext.onReady(function () {
     //    "http://maps.opengeo.org/geowebcache/service/wms",
     //    {layers: "bluemarble"}
     //    );
-	//map.addLayer(layer);
-	
-	var gmap = new OpenLayers.Layer.Google(
-		"Google Streets", // the default
-		{numZoomLevels: 20}
-		// default type, no change needed here
-	);
-	var gphy = new OpenLayers.Layer.Google(
-		"Google Physical",
-		{type: google.maps.MapTypeId.TERRAIN}
-		// used to be {type: G_PHYSICAL_MAP}
-	);
-	var osm = new OpenLayers.Layer.OSM();
-	var ghyb = new OpenLayers.Layer.Google(
-		"Google Hybrid",
-		{type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}
-		// used to be {type: G_HYBRID_MAP, numZoomLevels: 20}
-	);
-	var gsat = new OpenLayers.Layer.Google(
-		"Google Satellite",
-		{type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
-		// used to be {type: G_SATELLITE_MAP, numZoomLevels: 22}
-	);
+    //map.addLayer(layer);
 
-	map.addLayers([gmap,gphy,osm, ghyb, gsat]);
-	map.addControl( new OpenLayers.Control.LayerSwitcher);
+    var gmap = new OpenLayers.Layer.Google(
+        "Google Streets", // the default
+        {numZoomLevels: 20}
+        // default type, no change needed here
+        );
+    var gphy = new OpenLayers.Layer.Google(
+        "Google Physical",
+        {type: google.maps.MapTypeId.TERRAIN}
+        // used to be {type: G_PHYSICAL_MAP}
+        );
+    var osm = new OpenLayers.Layer.OSM();
+    var ghyb = new OpenLayers.Layer.Google(
+        "Google Hybrid",
+        {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}
+        // used to be {type: G_HYBRID_MAP, numZoomLevels: 20}
+        );
+    var gsat = new OpenLayers.Layer.Google(
+            "Google Satellite",
+            {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
+            // used to be {type: G_SATELLITE_MAP, numZoomLevels: 22}
+            );
+
+    map.addLayers([gmap,gphy,osm, ghyb, gsat]);
+    map.addControl( new OpenLayers.Control.LayerSwitcher);
 
     mapPanel = new GeoExt.MapPanel({
         map: map,
-        region: 'center',
-		zoom: 6,
-		tbar: new Ext.Toolbar(),
-		center:new OpenLayers.LonLat(11.019287, 51.041394).transform(
-        	new OpenLayers.Projection("EPSG:4326"),
-     	   map.getProjectionObject())
+             region: 'center',
+             zoom: 6,
+             tbar: new Ext.Toolbar(),
+             center:new OpenLayers.LonLat(11.019287, 51.041394).transform(
+                 new OpenLayers.Projection("EPSG:4326"),
+                 map.getProjectionObject())
     });
 
-   new Ext.Panel({
+    new Ext.Panel({
         title: "Elevation Profile",
         layout: 'border',
         renderTo: 'gxmap',
@@ -50,67 +50,66 @@ Ext.onReady(function () {
         //width: 800,
         tbar: new Ext.Toolbar({
             items: [
-                {text: 'func1', handler: function() { alert('func1'); } },
-                {text: 'func2', handler: function() { alert('func2'); } },
-            ]
+        {text: 'func1', handler: function() { alert('func1'); } },
+        {text: 'func2', handler: function() { alert('func2'); } },
+        ]
         }),
         items: [mapPanel]
     });
-	
-//just a litte test (Paul)
-		var length = new OpenLayers.Control.Measure(OpenLayers.Handler.Path, {
-    eventListeners: {
-        measure: function(evt) {
-            alert("The length was " + evt.measure + evt.units);
+
+    //just a litte test (Paul)
+    var length = new OpenLayers.Control.Measure(OpenLayers.Handler.Path, {
+        eventListeners: {
+                            measure: function(evt) {
+                                         alert("The length was " + evt.measure + evt.units);
+                                     }
+                        }
+    });
+
+
+    var area = new OpenLayers.Control.Measure(OpenLayers.Handler.Polygon, {
+        eventListeners: {
+                            measure: function(evt) {
+                                         alert("The area was " + evt.measure + evt.units);
+                                     }
+                        }
+    });
+
+    mapPanel.map.addControl(length);
+    mapPanel.map.addControl(area);
+
+    var toggleGroup = "measure controls";
+
+    var lengthButton = new Ext.Button({
+        text: 'Measure Length',
+        enableToggle: true,
+        toggleGroup: toggleGroup,
+        toggleHandler: function toggleHandler(item, pressed) {
+            if (pressed) {
+                length.activate();
+            } else {
+                length.deactivate();
+            }
         }
-    }
-});
+    });
 
-
-var area = new OpenLayers.Control.Measure(OpenLayers.Handler.Polygon, {
-    eventListeners: {
-        measure: function(evt) {
-            alert("The area was " + evt.measure + evt.units);
+    var areaButton = new Ext.Button({
+        text: 'Measure Area',
+        enableToggle: true,
+        toggleGroup: toggleGroup,
+        toggleHandler: function toggleHandler(item, pressed) {
+            //Ext.Msg.alert('toggleHandler', 'toggle pressed');
+            if (pressed) {
+                area.activate();
+            } else {
+                area.deactivate();
+            }
         }
-    }
-});
 
-mapPanel.map.addControl(length);
-mapPanel.map.addControl(area);
+    });
 
-var toggleGroup = "measure controls";
-
-var lengthButton = new Ext.Button({
-    text: 'Measure Length',
-    enableToggle: true,
-    toggleGroup: toggleGroup,
-    toggleHandler: function toggleHandler(item, pressed) {
-		 if (pressed) {
-            length.activate();
-        } else {
-            length.deactivate();
-        }
-	}
-});
-
-var areaButton = new Ext.Button({
-    text: 'Measure Area',
-    enableToggle: true,
-    toggleGroup: toggleGroup,
-    toggleHandler: function toggleHandler(item, pressed) {
- 		//Ext.Msg.alert('toggleHandler', 'toggle pressed');
-		 if (pressed) {
-            area.activate();
-        } else {
-            area.deactivate();
-        }
-	}
-
-});
-		
-		mapPanel.getTopToolbar().addButton(lengthButton);
-		mapPanel.getTopToolbar().addButton(areaButton);
-
+    mapPanel.getTopToolbar().addButton(lengthButton);
+    mapPanel.getTopToolbar().addButton(areaButton);
 
 });
 
